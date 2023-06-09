27aa3dab9a2c9b5ca9cf4a63537082b1c5376520
Alekse <aplay675@gmail.com>
2023-06-09 15:13:18 +0300

    1 day

---
+++ miniblog/blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Post(models.Model):
+    title = models.CharField('Заголовок записи', max_length=100)
+    description = models.TextField('Текст', null=True)
+    author = models.CharField('Автор', max_length=100)
+    date = models.DateField('Дата', null=True)
+
+    def __str__(self):
+        return f'{self.title}, {self.author}'
+
+    class Meta:
+        verbose_name = 'Запись'
+        verbose_name_plural = 'Записи'
